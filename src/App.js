311af43d890e182fb6c1ca3445d942c230f13920
FicusPlol <rambler124413@gmail.com>
2024-03-06 14:18:15 +0300

    111

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {
 
-function App() {
+  Card,
+  ListGroup,
+
+  } from "react-bootstrap";
+
+  const Doctor ={
+    name: 'James Stron',
+    age: '49',
+    specialization: 'Cardiologist',
+    place:'St. Mary General Hospital',
+    
+}
+
+
+function DoctorCard() {
   return (
-    <div className="App">
-      
-      <h1> Hiiiшi</h1>
-    </div>
-   
+    <center>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://www.garant.ru/files/7/0/1291507/v-zemskie-doktora-smogut-poyti-vrachi-predpensionnogo-vozrasta_200.jpg" />
+      <Card.Body>
+        <Card.Title></Card.Title>
+        <Card.Text>
+        There is no point in saying that medicine plays an important role in the lives of all people.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Name: {Doctor.name}</ListGroup.Item>
+        <ListGroup.Item>Specialization: {Doctor.specialization}</ListGroup.Item>
+        <ListGroup.Item>Age: {Doctor.age}</ListGroup.Item>
+        <ListGroup.Item>{Doctor.place}</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="https://www.smgh.ca/">Link</Card.Link>
+        
+      </Card.Body>
+    </Card>
+    </center>
   );
 }
 
-export default App;
+export default DoctorCard;
